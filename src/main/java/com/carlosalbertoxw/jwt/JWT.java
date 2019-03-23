5793bfc62fddee9c507e09e5c4c1c96bebc9aa63
Carlos Alberto <carlosalbertoxw@gmail.com>
2019-03-23 17:18:24 -0600

    Se corrige detalle en firma de token

--- src/main/java/com/carlosalbertoxw/jwt/JWT.java
+++ src/main/java/com/carlosalbertoxw/jwt/JWT.java
@@ -21,19 +21,24 @@ public class JWT {
     private static final String KEY = "qwerty";
 
     public String createJWT(Integer IdUser) {
-        Date date = new Date();
-        date.setTime(date.getTime() + 1000 * 60 * 1);
-        String compactJws = Jwts.builder()
-                .setExpiration(date)
-                .claim("id", IdUser)
-                .signWith(SignatureAlgorithm.HS512, KEY)
-                .compact();
-        return compactJws;
+        try {
+            Date date = new Date();
+            date.setTime(date.getTime() + 1000 * 60 * 1);
+            String compactJws = Jwts.builder()
+                    .setExpiration(date)
+                    .claim("id", IdUser)
+                    .signWith(SignatureAlgorithm.HS512, KEY.getBytes("UTF-8"))
+                    .compact();
+            return compactJws;
+        } catch (Exception e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
     }
 
     public Integer verifyJWT(String compactJws) {
         try {
-            Claims claims = Jwts.parser().setSigningKey(KEY).parseClaimsJws(compactJws).getBody();
+            Claims claims = Jwts.parser().setSigningKey(KEY.getBytes("UTF-8")).parseClaimsJws(compactJws).getBody();
             if (claims.getExpiration().after(new Date())) {
                 if (claims.get("id") != null && !claims.get("id").equals("")) {
                     return (Integer) claims.get("id");
